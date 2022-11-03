6bbe5031b253630247c04ce301ec9664e0babfb0
senjaputras <100180101+senjaputras@users.noreply.github.com>
2022-11-03 12:36:30 +0800

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -23,7 +23,7 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 		wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 		echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
-	    echo ""
+	    	echo ""
 		echo "====== INSTALLING NEWEST ELK STACK ON Ubuntu OS ======"
 		echo ""
 	        wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
