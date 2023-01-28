be156b77fc7d70089e3f21f51df3be02bbc2842e
senjaputras <100180101+senjaputras@users.noreply.github.com>
2023-01-29 04:51:35 +0700

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -1,4 +1,3 @@
-echo "======== ELK STACK SETUP! Credit:PUTRAS  ========"
 echo "========TAKE A NOTE YOU IP ADDRESS FIRST PLEASE ========"
 echo "Y for install, N for UNINSTALL! "
 read inorun
@@ -14,15 +13,9 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 		apt-get install unzip
 		sudo apt-get install apt-transport-https
 		sudo apt-get update
-		echo "INSTALL ELASTIC-AGENT? Y/N "
-		read agent
-			if [[ $agent == "y" || $agent == "Y" ]]
-				then
-				wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.4.0-amd64.deb
-			fi
+		echo "====== Overwrite Keyring if Exist ====="
 		wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 		echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-
 	    	echo ""
 		echo "====== INSTALLING NEWEST ELK STACK ON Ubuntu OS ======"
 		echo ""
@@ -30,25 +23,28 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 		echo "====== INSTALL AND CONFIGURING ELASTICSEARCH ====="
 		echo ""
 		apt-get update
-	        apt-get install elasticsearch > secinfo
+		echo "DOWNLOAD ELK? Y or N"
+		read download
+		echo "WHICH VERSION ELK STACK?"
+		read ELK_VERS
+		if [[ $download == "y" || $download == "Y" ]]
+			then
+			curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELK_VERS}-amd64.deb
+			curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-${ELK_VERS}-amd64.deb
+		fi
+	        dpkg -i elasticsearch-${ELK_VERS}-amd64.deb | grep "generated password" > secinfo
 	        systemctl daemon-reload; systemctl enable elasticsearch.service
-		es_ip_def=$(cat /etc/elasticsearch/elasticsearch.yml | grep  '#network.host: ')
-		echo "INPUT YOUR ELASTICSEARCH & KIBANA IP ADDRESS : "
+		#es_ip_def=$(cat /etc/elasticsearch/elasticsearch.yml | grep  '#network.host: ')
+		echo "INPUT YOUR KIBANA IP ADDRESS : "
 		read ip_cst
 		es_port_def=$(cat /etc/elasticsearch/elasticsearch.yml | grep  '#http.port: ')
 		mv secinfo /etc/elasticsearch/secinfo
-		cd /etc/elasticsearch/
-		if [[ ! -z "$es_ip_def" ]]
-			then
-			echo "IP DEFAULT ON elasticsearch.yml CHANGE TO IP DEVICE"
-			sed -i '56d' elasticsearch.yml
-			sed -i "56 i network\.host\: $ip_cst" elasticsearch.yml
-		fi
+		#cd /etc/elasticsearch/
 		if [[ $es_port_def == *"http.port: 9200"* ]]
 			then
 			echo "ELASTICSEARCH DEFAULT PORT FOUND, CUSTOM YOURS ELASTICSEARCH PORT !"
 			read es_port_cst
-			sed -i "s/$es_port_def/http\.port\: $es_port_cst/g" elasticsearch.yml
+			sed -i "s/$es_port_def/http\.port\: $es_port_cst/g" /etc/elasticsearch/elasticsearch.yml
 		fi
                 echo "====== CHECK file /etc/elasticsearch/secinfo (CREDENTIAL)!! ====="
 		echo "====== CHECK FILE /etc/elasticsearch/elasticsearch.yml LATER, JUST MAKESURE network.host AND http.port !! ====="
@@ -57,9 +53,9 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 		echo  "====== ElasticSearch DONE ======"
 		echo ""
 		echo "====== INSTALL & CONFIGURING KIBANA ====="
-		apt-get install kibana
+		dpkg -i kibana-${ELK_VERS}-amd64.deb
 		systemctl daemon-reload; systemctl start kibana; systemctl enable kibana
-		cd /etc/kibana
+		#cd /etc/kibana
 	        kb_ip_def=$(cat /etc/kibana/kibana.yml | grep  'server.host: ')
 	        kb_port_def=$(cat /etc/kibana/kibana.yml | grep  'server.port: ')
 
@@ -67,14 +63,14 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 			then
 			echo ""
 			sed -i '15d' kibana.yml
-			sed -i "15 i server\.host\: $ip_cst" kibana.yml
+		  	sed -i "15 i server\.host\: $ip_cst" /etc/kibana/kibana.yml
 		fi
 	        if [[ $kb_port_def != "server.port: "* ]]
 	                then
 	                echo "====== KIBANA server.port USE DEFAULT PORT, YOU CAN CHANGE TO ANOTHER PORT! CHOOSE CUSTOM PORT ! ====== "
 	                read kb_port_cst
-			kb_port_line=$(awk '/server.port: 5601/{print NR}' kibana.yml)
-			sed -i "$kb_port_line i server\.port\: $kb_port_cst" kibana.yml
+			kb_port_line=$(awk '/server.port: 5601/{print NR}' /etc/kibana/kibana.yml)
+			sed -i "$kb_port_line i server\.port\: $kb_port_cst" /etc/kibana/kibana.yml
 		else
 			echo "THERE ARE ANY PORT HAS BEEN OPEN"
 	 	fi
@@ -83,11 +79,9 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 		echo ""
 		cd /usr/share/elasticsearch
                 echo "====== GENERATING CERTIFICATE FOR FLEET-SERVER ======"
-		echo "====== SAVE AS ca.zip ! ======"
-		./bin/elasticsearch-certutil ca --pem; unzip ca.zip
+		echo | ./bin/elasticsearch-certutil ca --pem; unzip elastic-stack-ca.zip
 		echo ""
-		echo "====== NOW, SAVE AS fleet-server.zip ! ======"
-		./bin/elasticsearch-certutil cert --name fleet-server --ca-cert /usr/share/elasticsearch/ca/ca.crt --ca-key /usr/share/elasticsearch/ca/ca.key --ip $ip_cst --pem; unzip fleet-server.zip
+		echo | ./bin/elasticsearch-certutil cert --name fleet-server --ca-cert /usr/share/elasticsearch/ca/ca.crt --ca-key /usr/share/elasticsearch/ca/ca.key --ip $ip_cst --pem; unzip certificate-bundle.zip
 		echo ""
 		echo "======  CERTIFICATE DIRECTORY IS ON /usr/share/elasticsearch/... ====== "
 		echo ""
@@ -95,17 +89,21 @@ if [[ $inorun == "y" || $inorun == "Y" ]]
 		echo "COPY THE TOKEN"
 		echo "AND PASTE BELOW!"; cd /usr/share/kibana/bin
 		./kibana-setup 
-		/usr/share/bin/kibana-encryption-keys generate | grep "encryptionKey:" >> /etc/kibana/kibana.yml
+		/usr/share/kibana/bin/kibana-encryption-keys generate | grep "encryptionKey:" >> /etc/kibana/kibana.yml
 		service kibana restart
 		echo "INSTALL ELASTIC-AGENT? Y/N "
 		read agent
 			if [[ $agent == "y" || $agent == "Y" ]]
 				then
-				apt-get install elastic-agent
+				echo ${ip_cst}
+				echo ${es_port_cst}
+				echo ${kb_port_cst}
+				curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-${ELK_VERS}-linux-x86_64.tar.gz
+				sudo tar xzvf elastic-agent-${ELK_VERS}-linux-x86_64.tar.gz
+                                cd elastic-agent-${ELK_VERS}-linux-x86_64
+                                #sudo ./elastic-agent install --fleet-server-es=https://$ip_cst:$es_port_cst --fleet-server-service-token=$(curl -k -u "elastic:$(awk -F ' : ' {'print $2'} < /etc/elasticsearch/secinfo | tr -d " \t\n\r")" -s -X POST http://$ip_cst:$kb_port_cst/api/fleet/service-tokens --header 'kbn-xsrf: true' | jq -r .value) --fleet-server-policy=fleet-server-policy --fleet-server-es-ca-trusted-fingerprint=$(sudo openssl x509 -fingerprint -sha256 -noout -in /etc/elasticsearch/certs/http_ca.crt | awk -F"=" {' print $2 '} | sed s/://g)
+				sudo ./elastic-agent install --fleet-server-es=https://${ip_cst}:${es_port_cst} --fleet-server-service-token=$(curl -k -u "elastic:$(awk -F ' : ' {'print $2'} < /etc/elasticsearch/secinfo | tr -d " \t\n\r")" -s -X POST http://{ip_cst}:${kb_port_cst}/api/fleet/service-tokens --header 'kbn-xsrf: true' | jq -r .value) --fleet-server-policy=fleet-server-policy --fleet-server-es-ca-trusted-fingerprint=$(sudo openssl x509 -fingerprint -sha256 -noout -in /etc/elasticsearch/certs/http_ca.crt | awk -F"=" {' print $2 '} | sed s/://g)
 			fi
-		wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-
-
 	else
 	echo "CentOS"
 	fi
